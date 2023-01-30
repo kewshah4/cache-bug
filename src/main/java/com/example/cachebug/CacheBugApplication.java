50a784d7bc356f24b1a55a87065d68a5b5b7d32b
Kewal Shah <kewshah@expediagroup.com>
2023-01-30 12:04:50 +0530

    initial commit

---
+++ src/main/java/com/example/cachebug/CacheBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheBugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cachebug/CacheBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cachebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
